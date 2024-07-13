bf81b97c770aab9318a761b1d0dd76a6433c1866
serendipity552689 <bill1666@qq.com>
2024-07-13 07:51:26 +0100

    Site updated: 2024-07-13 07:51:25

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1720853210038";
+const VERSION = "1720853360353";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
